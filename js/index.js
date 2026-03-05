4e967d708fabb5e93f8aaf95d25f9c34e5c592fe
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2026-03-05 20:42:08 +0600

    add spinned for loading purpose

--- js/index.js
+++ js/index.js
@@ -3,6 +3,16 @@ const createElements = (arr) => {
   return htmlElements.join(" ");
 };
 
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("spinner").classList.add("hidden");
+    document.getElementById("word-container").classList.remove("hidden");
+  }
+};
+
 const loadLessons = () => {
   const url = "https://openapi.programming-hero.com/api/levels/all";
   fetch(url)
@@ -18,6 +28,7 @@ const removeActive = () => {
 };
 
 const loadLevelWord = (id) => {
+  manageSpinner(true);
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
@@ -63,6 +74,7 @@ const displayLevelWord = (words) => {
         </h2>
       </div>
     `;
+    manageSpinner(false);
     return;
   } else {
     words.forEach((word) => {
@@ -84,6 +96,7 @@ const displayLevelWord = (words) => {
       wordContainer.append(card);
     });
   }
+  manageSpinner(false);
 };
 
 const displayWordDetails = (word) => {
